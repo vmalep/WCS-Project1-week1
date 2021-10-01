54d2d0083c95b48e4c5cdbfa37f93f09b3ebf443
Pierre van Male <pierre-van-male_student2021@wilder.school>
2021-10-02 00:05:27 +0200

    Initial commit

---
+++ src/city.js
@@ -0,0 +1,13 @@
+// For the header
+const menuBtn = document.getElementById("menu-btn");
+
+menuBtn.addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("menu-on");
+});
+
+/*const joinBtn1 = document.getElementsByClassName("buttonevent1");
+joinBtn1.addEventListener("click", function() {
+
+})
+*/
